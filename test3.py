483407f842521b23b3a8f713617cea4d0e64a327
maestromaximo <alejandrogarcia2423@hotmail.com>
2024-08-21 22:35:05 -0400

    cont

--- test3.py
+++ test3.py
@@ -127,7 +127,7 @@ else:
 running = True
 while running:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT or event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
+        if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
             running = False
 
 # Cleanup
